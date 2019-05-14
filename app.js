80e34c96a681fc2afd52b9dda4fcdd5429c3d8a0
Sagar Gavhane <sagar.gavhane@parambs.com>
2019-05-14 16:39:57 +0530

    ...

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
 
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
+const indexRouter = require('./routes/index')
 
-var app = express()
+const app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -15,6 +16,5 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 module.exports = app
